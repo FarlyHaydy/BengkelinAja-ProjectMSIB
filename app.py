6427f206bab2577b016503aef33f9bd140f016eb
M.AKHMAL <137740587+Akhmall@users.noreply.github.com>
2024-11-25 23:02:45 +0700

    update

--- app.py
+++ app.py
@@ -2,9 +2,26 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods=['GET'], endpoint='home')
 def home():
-    return render_template('index.html')
+    return render_template('user/hompage.html')
+
+@app.route('/produk')
+def produk():
+    return render_template('user/produk.html') 
+
+@app.route('/checkout')
+def checkout():
+    return render_template('user/checkout.html')
+ 
+@app.route('/check_order')
+def check_order():
+    return render_template('user/check_order.html') 
+
+@app.route('/profile')
+def profile():
+    return render_template('user/profile.html') 
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
